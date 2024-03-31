a01c94d11c182145456d3ac4e89afa0811276f39
fredm23579 <motta@g.ucla.edu>
2024-03-30 20:52:33 -0700

    updated index.js svg code

--- index.js
+++ index.js
@@ -1,70 +1,77 @@
 // index.js
-const inquirer = require('inquirer');
 const fs = require('fs');
 const { Triangle, Circle, Square } = require('./lib/shapes');
 
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'text',
-      message: 'Enter text for the logo (up to three characters):',
-      validate: function (value) {
-        return value.length <= 3 ? true : 'Please enter up to three characters.';
+// Import inquirer using dynamic import
+import('inquirer').then((inquirer) => {
+  function promptUser() {
+    return inquirer.default.prompt([
+      {
+        type: 'input',
+        name: 'text',
+        message: 'Enter text for the logo (up to three characters):',
+        validate: function (value) {
+          return value.length <= 3 ? true : 'Please enter up to three characters.';
+        }
+      },
+      {
+        type: 'input',
+        name: 'textColor',
+        message: 'Enter text color (color keyword or hexadecimal):',
+        default: '#000000' // Default to black
+      },
+      {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['Triangle', 'Circle', 'Square']
+      },
+      {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter shape color (color keyword or hexadecimal):',
+        default: '#FFFFFF' // Default to white
       }
-    },
-    {
-      type: 'input',
-      name: 'textColor',
-      message: 'Enter text color (color keyword or hexadecimal):',
-      default: '#000000' // Default to black
-    },
-    {
-      type: 'list',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['Triangle', 'Circle', 'Square']
-    },
-    {
-      type: 'input',
-      name: 'shapeColor',
-      message: 'Enter shape color (color keyword or hexadecimal):',
-      default: '#FFFFFF' // Default to white
-    }
-  ]);
-}
-
-function createLogo(answers) {
-  let shape;
-  switch (answers.shape) {
-    case 'Triangle':
-      shape = new Triangle();
-      break;
-    case 'Circle':
-      shape = new Circle();
-      break;
-    case 'Square':
-      shape = new Square();
-      break;
-    default:
-      console.log('Invalid shape.');
-      return;
+    ]);
   }
-  shape.setColor(answers.shapeColor);
 
-  const svgContent = `
-  <svg width="300" height="200">
-    ${shape.render()}
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
-  </svg>
-  `;
+  function createLogo(answers) {
+    let shape;
+    let shapeName;
+    switch (answers.shape) {
+      case 'Triangle':
+        shape = new Triangle();
+        shapeName = 'triangle';
+        break;
+      case 'Circle':
+        shape = new Circle();
+        shapeName = 'circle';
+        break;
+      case 'Square':
+        shape = new Square();
+        shapeName = 'square';
+        break;
+      default:
+        console.log('Invalid shape.');
+        return;
+    }
+    shape.setColor(answers.shapeColor);
 
-  saveSVG(svgContent);
-}
+    const svgContent = `
+    <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+    </svg>
+    `;
 
-function saveSVG(content) {
-  fs.writeFileSync('./examples/logo.svg', content);
-  console.log('Generated logo.svg');
-}
+    const fileName = `${shapeName}_${answers.textColor}_${answers.shapeColor}.svg`;
+    saveSVG(fileName, svgContent);
+  }
 
-promptUser().then(createLogo);
+  function saveSVG(fileName, content) {
+    fs.writeFileSync(`./examples/${fileName}`, content);
+    console.log(`Generated ${fileName}`);
+  }
+
+  promptUser().then(createLogo);
+});
